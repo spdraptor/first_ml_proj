56430e287ade96967c435a5e20df61c87de3e6c5
Sanket Dhuri <spdraptor.micro@outlook.com>
2023-05-21 18:01:21 +0530

    tutorial 4 : Project Problem Statement,EDA And Model Training

--- src/exception.py
+++ src/exception.py
@@ -13,15 +13,3 @@ class CustomException(Exception):
     
     def __str__(self) -> str:
         return self.error_msg
-
-
-if __name__ == "__main__":
-    
-    try:
-        a = 1/0
-    except Exception as e:
-        # logging.info("Divide by zero error")
-        logging.info("Divide by zero error")
-        raise CustomException(e,sys)
-
-    
